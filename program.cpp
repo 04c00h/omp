03a011c84326c5b217c6a7c19b75b7543c124573
04c00h <asimo92@yandex.ru>
2017-02-16 16:38:55 +0400

    Add files via upload

---
+++ program.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <omp.h>
+#include <climits>
+#include <stdlib.h>
+#include <ctime>
+
+const int NUM_THREADS = 4;
+const int N = 10000;
+const int M = 10000;
+int matrix[N][M];
+
+void min_matrix()
+{
+	long double clockStart, clockStop;
+	int i, j, tmpMin, tmpMax;
+
+	clockStart = omp_get_wtime();
+	//srand(time(0)); //выкл. для отладки, чтобы значения rand() повторялись
+
+	#pragma omp parallel for private(j) shared(i) num_threads(NUM_THREADS)
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < M; j++) {
+			matrix[i][j] = rand();
+		}
+	}
+
+	tmpMin = INT_MAX;
+
+	#pragma omp parallel for private(tmpMax, j) shared(tmpMin, i) num_threads(NUM_THREADS)
+	for (i = 0; i < N; i++) {
+		tmpMax = matrix[i][0];
+		for (j = 1; j < M; j++) {
+			if (tmpMax < matrix[i][j])
+				tmpMax = matrix[i][j];
+		}
+		#pragma omp critical (minvalue)
+		{
+		if (tmpMin > tmpMax)
+			tmpMin = tmpMax;
+		}
+	}
+
+	clockStop = omp_get_wtime();
+	
+	printf("MIN = %d\n", tmpMin);
+	printf("Seconds: %f\n", clockStop - clockStart);
+}
+
+int main()
+{
+	min_matrix();
+
+	getchar();
+	return 0;
+}
